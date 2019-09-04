9723d75a621f24f78746e083d10fd8cd3f39be8a
irwin.jiangwei <17735528@qq.com>
2019-09-04 11:17:40 +0800

    Create 9.palindrome-number.py

---
+++ 9.palindrome-number.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+# https://leetcode.com/problems/palindrome-number/description/
+#
+# algorithms
+# Easy (44.68%)
+# Likes:    1601
+# Dislikes: 1366
+# Total Accepted:    662.7K
+# Total Submissions: 1.5M
+# Testcase Example:  '121'
+#
+# Determine whether an integer is a palindrome. An integer is a palindrome when
+# it reads the same backward as forward.
+# 
+# Example 1:
+# 
+# 
+# Input: 121
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it
+# becomes 121-. Therefore it is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+# 
+# 
+# Follow up:
+# 
+# Coud you solve it without converting the integer to a string?
+# 
+#
+class Solution:
+    def isPalindrome(self, n: int) -> bool:
+        if n < 0:
+            return False
+
+        temp=n
+        rev=0
+        while(n>0):
+            dig = n%10          # get the last digit by % 10
+            rev = rev*10+dig
+            n = n//10           # remove the last digit by // 10
+        if(temp==rev):
+            return True
+        else:
+            return False
+
